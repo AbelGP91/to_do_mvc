297d6234b2e47413416d3ef77fb2dc39c90cf511
Abel Galindo Pascual <abel.gp.1991@hotmail.es>
2023-07-18 12:17:23 +0200

    Conexion BDD + Nombres Lista

--- app/models/Tareas.php
+++ app/models/Tareas.php
@@ -2,19 +2,22 @@
 
 class Tareas extends Model {
 
-    
+    private $conexion;    
 
     public function __construct(){
 
         parent::__construct();
-        $this->_setTable('tasques');
+
+        $this->conexion = new mysqli("localhost","root","","to_do");
+
+        $this->_setTable('tasques');      
                 
     }
 
     
     public function findTareasById($id){
 
-        $mysql = new mysqli("localhost","root","","to_do");
+        $mysql = $this->conexion;
 
         $sql = "SELECT idTasques, nom_tasques, descrip_tasques, estat_tasques, inici_tasques, fi_tasques FROM to_do.tasques WHERE tasques.Usuario_idUsuario LIKE '$id'" 
             or die($mysql->error);
@@ -41,7 +44,7 @@ class Tareas extends Model {
 
     public function addData($data){
 
-        $mysql = new mysqli("localhost","root","","to_do");
+        $mysql = $this->conexion;
 
         $sql = "INSERT INTO to_do.tasques(nom_tasques,descrip_tasques,estat_tasques,inici_tasques,fi_tasques,Usuario_idUsuario) 
         VALUES ('$data[0]','$data[1]','$data[4]','$data[2]','$data[3]','$data[5]')";
@@ -55,7 +58,7 @@ class Tareas extends Model {
 
     public function newData($newData){       
 
-        $mysql = new mysqli("localhost","root","","to_do");
+        $mysql = $this->conexion;
 
         if ($newData['titulo'] !=null){
 
@@ -98,7 +101,7 @@ class Tareas extends Model {
 
     public function deleteData($idTasca){
 
-        $mysql = new mysqli("localhost","root","","to_do");
+        $mysql = $this->conexion;
 
         $sql = "DELETE FROM to_do.tasques WHERE idTasques = '$idTasca'";
         $mysql->query($sql);
